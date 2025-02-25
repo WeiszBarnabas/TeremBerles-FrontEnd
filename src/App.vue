85c8ff57fd26c16145ae586fc5915eab45fc994c
Aerovity <andras.banfi.hun@gmail.com>
2025-02-25 15:14:00 +0100

    navbar-start

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <RouterView />
+  <div>
+    <NavBar />
+    <RouterView />
+  </div>
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router'
+import NavBar from './components/Navbar.vue'
 
 </script>
 
